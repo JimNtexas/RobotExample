bf02ec79e797f948aef9ea00b49af09f07d44d5a
JimNtexas <grayraven42@gmail.com>
2016-07-03 22:14:18 -0500

    initial commit

---
+++ app/src/main/java/com/grayraven/robotexample/MainActivity.java
@@ -0,0 +1,86 @@
+package com.grayraven.robotexample;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    Intent mService;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        mService = new Intent(this, MyService.class);
+        startService(mService);
+
+    }
+
+    // This method will be called when a MessageEvent is posted
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    public void onMessageEvent(MessageEvent event) {
+       Log.d(TAG, "event received: " + event.message);
+        final String msg = event.message;
+        MainActivity.this.runOnUiThread(new Runnable() {
+            //Eventbus handlers need to return quickly, so show the toast asynchronously
+            public void run() {
+                Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+
+
+
+    }
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        EventBus.getDefault().register(this);
+    }
+
+    @Override
+    public void onStop() {
+        EventBus.getDefault().unregister(this);
+        stopService(mService);
+        super.onStop();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/grayraven/robotexample/MyService.java
@@ -0,0 +1,68 @@
+package com.grayraven.robotexample;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+import org.greenrobot.eventbus.EventBus;
+
+public class MyService extends Service {
+    private static final String TAG ="MyService";
+    private boolean isRunning  = false;
+    public MyService() {
+    }
+
+    @Override
+    public void onCreate() {
+        isRunning  = false;
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+
+        Log.i(TAG, "Service onStartCommand");
+
+        //Creating new thread for my service
+        //Always write your long running tasks in a separate thread, to avoid ANR
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                int cnt = 0;
+                if(isRunning){
+                    Log.i(TAG, "Service running");
+                }
+
+                //NOTE: See this SO post about the traditional ways to send a message from a service to an activity:
+                //http://stackoverflow.com/questions/12997463/send-intent-from-service-to-activity
+                //I think using an event bus is less trouble.
+
+                while(true) {
+                    try {
+                        Thread.sleep(5000);
+                        //send toast message to main activity
+                        String msg = "Service count: " + cnt;
+                        Log.d(TAG, "message posted: " + cnt);
+                        EventBus.getDefault().post(new MessageEvent(msg));
+                        cnt++;
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
+
+        return Service.START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        isRunning = false;
+        Log.i(TAG, "Service onDestroy");
+    }
+}
